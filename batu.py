9b04f69ee5477a83603d8bd4044e99ccf0d1411a
Just-Batu <147743211+Just-Batu@users.noreply.github.com>
2023-11-05 20:40:52 +0300

    batu.py

---
+++ batu.py
@@ -0,0 +1,20 @@
+import requests
+def send_discord_webhook(webhook_url, message, num_messages):
+    data = {
+        "content": message
+    }
+    
+    for _ in range(num_messages):
+        response = requests.post(webhook_url, json=data)
+        
+        if response.status_code == 204:
+            print("Mesaj gönderildi.")
+        else:
+            print("Hata oluştu. Mesaj gönderilemedi.")
+
+if __name__ == "__main__":
+    webhook_url = input("Webhook URL'sini girin: ")
+    message = input("Göndermek istediğiniz mesajı girin: ")
+    num_messages = int(input("Kaç kez mesaj göndermek istiyorsunuz: "))
+
+    send_discord_webhook(webhook_url, message, num_messages)
